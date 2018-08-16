d321192b5a8d1ce401e4806cf455da2b4e369a3d
kyle-c-simmons <kylesimmons96@protonmail.com>
2018-08-16 22:13:03 +0100

    homepage update

--- frontend/src/components/Pages/HomePage.vue
+++ frontend/src/components/Pages/HomePage.vue
@@ -1,6 +1,6 @@
 <template>
     <div >
-      <div id="myCarousel" class="carousel slide" data-ride="carousel" data-interval="4000">
+      <div id="myCarousel" class="carousel slide" data-ride="carousel" data-interval="3500">
         <!-- Indicators -->
         <ol class="carousel-indicators">
           <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
@@ -9,7 +9,7 @@
         </ol>
 
         <!-- Wrapper for slides -->
-        <div class="carousel-inner">
+        <div id="home-sldier" class="carousel-inner">
           <div class="item active">
             <img src="../../images/image-slide-1.jpg" alt="Los Angeles">
           </div>
@@ -34,65 +34,63 @@
         </a>
       </div>
 
-<div class="jumbotron">
-    <h2>Top 6 Movies</h2><br><br>
-      <div class="row">
+  <div class="jumbotron">
+      <h2>Top 6 Movies</h2>
+        <div class="row">
 
-    <div class="col-md-12">
+      <div class="col-md-12">
 
-        <div id="mdb-lightbox-ui"></div>
+          <div id="mdb-lightbox-ui"></div>
 
-        <div id="top-6-movies" class="mdb-lightbox no-margin">
+          <div id="top-6-movies" class="mdb-lightbox no-margin">
 
-            <figure class="col-md-4">
-                <a class="black-text" href="https://mdbootstrap.com/img/Photos/Horizontal/Nature/12-col/img%20(121).jpg" data-size="1600x1067">
-                    <img alt="picture" src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(121).jpg" class="img-fluid">
-                    <h3 class="text-center my-3">Photo title</h3>
-                </a>
-            </figure>
+              <figure class="col-md-4">
+                  <a class="black-text" href="https://mdbootstrap.com/img/Photos/Horizontal/Nature/12-col/img%20(121).jpg" data-size="1600x1067">
+                      <img alt="picture" src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(121).jpg" class="img-fluid">
+                      <h3 class="text-center my-3">Photo title</h3>
+                  </a>
+              </figure>
 
-            <figure class="col-md-4">
-                <a class="black-text" href="https://mdbootstrap.com/img/Photos/Horizontal/Nature/12-col/img%20(96).jpg" data-size="1600x1067">
-                    <img alt="picture" src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(96).jpg" class="img-fluid" />
-                    <h3 class="text-center my-3">Photo title</h3>
-                </a>
-            </figure>
+              <figure class="col-md-4">
+                  <a class="black-text" href="https://mdbootstrap.com/img/Photos/Horizontal/Nature/12-col/img%20(96).jpg" data-size="1600x1067">
+                      <img alt="picture" src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(96).jpg" class="img-fluid" />
+                      <h3 class="text-center my-3">Photo title</h3>
+                  </a>
+              </figure>
 
-            <figure class="col-md-4">
-                <a class="black-text" href="https://mdbootstrap.com/img/Photos/Horizontal/Nature/12-col/img%20(104).jpg" data-size="1600x1067">
-                    <img alt="picture" src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(104).jpg" class="img-fluid" />
-                    <h3 class="text-center my-3">Photo title</h3>
-                </a>
-            </figure>
+              <figure class="col-md-4">
+                  <a class="black-text" href="https://mdbootstrap.com/img/Photos/Horizontal/Nature/12-col/img%20(104).jpg" data-size="1600x1067">
+                      <img alt="picture" src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(104).jpg" class="img-fluid" />
+                      <h3 class="text-center my-3">Photo title</h3>
+                  </a>
+              </figure>
 
-            <figure class="col-md-4">
-                <a class="black-text" href="https://mdbootstrap.com/img/Photos/Horizontal/Nature/12-col/img%20(45).jpg" data-size="1600x1067">
-                    <img alt="picture" src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(45).jpg" class="img-fluid" />
-                    <h3 class="text-center my-3">Photo title</h3>
-                </a>
-            </figure>
+              <figure class="col-md-4">
+                  <a class="black-text" href="https://mdbootstrap.com/img/Photos/Horizontal/Nature/12-col/img%20(45).jpg" data-size="1600x1067">
+                      <img alt="picture" src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(45).jpg" class="img-fluid" />
+                      <h3 class="text-center my-3">Photo title</h3>
+                  </a>
+              </figure>
 
-            <figure class="col-md-4">
-                <a class="black-text" href="https://mdbootstrap.com/img/Photos/Horizontal/Nature/12-col/img%20(122).jpg" data-size="1600x1067">
-                    <img alt="picture" src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(122).jpg" class="img-fluid" />
-                    <h3 class="text-center my-3">Photo title</h3>
-                </a>
-            </figure>
+              <figure class="col-md-4">
+                  <a class="black-text" href="https://mdbootstrap.com/img/Photos/Horizontal/Nature/12-col/img%20(122).jpg" data-size="1600x1067">
+                      <img alt="picture" src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(122).jpg" class="img-fluid" />
+                      <h3 class="text-center my-3">Photo title</h3>
+                  </a>
+              </figure>
 
-            <figure class="col-md-4">
-                <a class="black-text" href="https://mdbootstrap.com/img/Photos/Horizontal/Nature/12-col/img%20(115).jpg" data-size="1600x1067">
-                    <img alt="picture" src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(115).jpg" class="img-fluid" />
-                    <h3 class="text-center my-3">Photo title</h3>
-                </a>
-            </figure>
-        </div>
-        <button id="movies-button" type="button" class="btn btn-primary">More Movies</button>
+              <figure class="col-md-4">
+                  <a class="black-text" href="https://mdbootstrap.com/img/Photos/Horizontal/Nature/12-col/img%20(115).jpg" data-size="1600x1067">
+                      <img alt="picture" src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(115).jpg" class="img-fluid" />
+                      <h3 class="text-center my-3">Photo title</h3>
+                  </a>
+              </figure>
+          </div>
+          <button id="movies-button" type="button" class="btn btn-primary">More Movies</button>
+      </div>
     </div>
-
+  </div>
 </div>
-</div>
-</div>
-
 
 </template>
 
@@ -100,35 +98,41 @@
 </script>
 
 <style>
-.carousel-inner {
-  width:640;
-  height:500px;
-}
-.carousel-inner .item {
-  opacity: 0;
-  -webkit-transition-property: opacity;
-  -moz-transition-property: opacity;
-  -o-transition-property: opacity;
-  transition-property: opacity;
-}
-.carousel-inner .active {
-  opacity: 1;
-}
-.carousel-inner .active.left,
-.carousel-inner .active.right {
-  left: 0;
-  opacity: 0;
-  z-index: 1;
-}
-.carousel-inner .next.left,
-.carousel-inner .prev.right {
-  opacity: 1;
-}
-.carousel-fade .carousel-control {
-  z-index: 2;
-}
+  .carousel-inner {
+    width: 100%;
+    height:550px;
+  }
+  .carousel-inner .item {
+    opacity: 0;
+    -webkit-transition-property: opacity;
+    -moz-transition-property: opacity;
+    -o-transition-property: opacity;
+    transition-property: opacity;
+  }
+  .carousel-inner .active {
+    opacity: 1;
+  }
+  .carousel-inner .active.left,
+  .carousel-inner .active.right {
+    left: 0;
+    opacity: 0;
+    z-index: 1;
+  }
+  .carousel-inner .next.left,
+  .carousel-inner .prev.right {
+    opacity: 1;
+  }
+  .carousel-fade .carousel-control {
+    z-index: 2;
+  }
 
-#movies-button {
-  margin-top: 40px !important;
-}
+  #movies-button {
+    margin-top: 40px !important;
+  }
+
+  .item img {
+    height: 550px !important;
+    width: 100% !important;
+  }
+
 </style>
